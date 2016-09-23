8b5c4edd7c29a7c45adf56dffb43c25fc6b50ad5
RosireMaiz <rosire08@gmail.com>
2016-09-23 09:24:41 -0430

    metodo de paraticiacion por cantidad

--- src/ve/smile/dao/ParticipacionDAO.java
+++ src/ve/smile/dao/ParticipacionDAO.java
@@ -1,8 +1,19 @@
 package ve.smile.dao;
 
+import java.util.List;
+import javax.persistence.Query;
 import lights.core.googlecode.genericdao.dao.jpa.BaseDAO;
 import ve.smile.dto.Participacion;
 
 public class ParticipacionDAO extends BaseDAO<Participacion> {
+	
+	@SuppressWarnings("unchecked")
+	public List<Participacion> findParticipacionLimt(Integer cantidad) {
+		String sql = "select p from Participacion p order by p.idParticipacion ASC";
 
+		Query query = createQuery(sql);
+
+		return query.setMaxResults(cantidad).getResultList();
+
+	}
 }
