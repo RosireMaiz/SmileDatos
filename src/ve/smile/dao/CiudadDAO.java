f0c2c2b3bf0735a53782cbdb9ea30c073a4126f2
rafa7 <rafa.rodriguezur@gmail.com>
2016-09-28 22:48:30 -0400

    consulta de ciudad por estado

--- src/ve/smile/dao/CiudadDAO.java
+++ src/ve/smile/dao/CiudadDAO.java
@@ -1,8 +1,25 @@
 package ve.smile.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
 import lights.core.googlecode.genericdao.dao.jpa.BaseDAO;
 import ve.smile.dto.Ciudad;
 
 public class CiudadDAO extends BaseDAO<Ciudad> {
+	
+	@SuppressWarnings("unchecked")
+	public List<Ciudad> consultaCiudadPorEstado(Integer id) {
+		String sql = "select c from Ciudad c where c.fkEstado.idEstado = ?1";
+
+		Query query = createQuery(sql);
+
+		query.setParameter(1, id);
+		
+		return query.getResultList();
+
+	}
+
+
 
 }
