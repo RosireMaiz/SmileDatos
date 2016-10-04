6ad12d6be3faffeab524782e61219761cb5ba999
RosireMaiz <rosire08@gmail.com>
2016-10-04 07:40:26 -0400

    Modificaciones para Servicios de Contacto portal

--- src/ve/smile/dao/ComunidadDAO.java
+++ src/ve/smile/dao/ComunidadDAO.java
@@ -1,8 +1,20 @@
 package ve.smile.dao;
 
+import javax.persistence.Query;
+
 import lights.core.googlecode.genericdao.dao.jpa.BaseDAO;
 import ve.smile.dto.Comunidad;
 
 public class ComunidadDAO extends BaseDAO<Comunidad> {
 
+	public Comunidad findByCorreo(String correo) {
+
+		String sql = "select c from Comunidad c where c.correo = ?1";
+
+		Query query = createQuery(sql);
+
+		query.setParameter(1, correo);
+
+		return (Comunidad) query.getSingleResult();
+	}
 }
