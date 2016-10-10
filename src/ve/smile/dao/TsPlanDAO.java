9ec06cd8736667085d2974220e93071b40b8ccd5
jorgeavenda <jorgeavenda.11@gmail.com>
2016-10-10 18:15:39 -0400

    albunes de TS en Galeria

--- src/ve/smile/dao/TsPlanDAO.java
+++ src/ve/smile/dao/TsPlanDAO.java
@@ -1,8 +1,23 @@
 package ve.smile.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import lights.core.googlecode.genericdao.dao.jpa.BaseDAO;
 import ve.smile.dto.TsPlan;
 
 public class TsPlanDAO extends BaseDAO<TsPlan> {
 
+	@SuppressWarnings("unchecked")
+	public List<TsPlan> findTSPlanificadoAlbum(Integer album) {
+		String sql = "select e from TsPlan e where e.fkAlbum.idAlbum = ?1";
+
+		Query query = createQuery(sql);
+		
+		query.setParameter(1, album);
+
+		return query.getResultList();
+
+	}
 }
