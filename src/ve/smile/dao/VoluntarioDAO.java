30965cb823641a492b83dad9e1280b6784023713
willandherg <willandherg>
2016-10-11 06:57:39 -0400

    Reporte Voluntario

--- src/ve/smile/dao/VoluntarioDAO.java
+++ src/ve/smile/dao/VoluntarioDAO.java
@@ -1,8 +1,18 @@
 package ve.smile.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import lights.core.googlecode.genericdao.dao.jpa.BaseDAO;
+import ve.smile.dto.Actividad;
 import ve.smile.dto.Voluntario;
 
 public class VoluntarioDAO extends BaseDAO<Voluntario> {
+	@SuppressWarnings("unchecked")
+	public List<Voluntario> consultaVoluntariosParametrizado(String sql) {
+		Query query = createQuery(sql);
+		return query.getResultList();
+	}
 
 }
