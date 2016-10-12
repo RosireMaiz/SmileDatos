4ddfb015366facf146d346f2f93632513acf9f9b
RosireMaiz <rosire08@gmail.com>
2016-10-12 13:26:00 -0400

    Login para Android

--- src/ve/smile/dao/PersonaDAO.java
+++ src/ve/smile/dao/PersonaDAO.java
@@ -1,8 +1,17 @@
 package ve.smile.dao;
 
 import lights.core.googlecode.genericdao.dao.jpa.BaseDAO;
+import lights.core.googlecode.genericdao.search.Search;
 import ve.smile.dto.Persona;
 
 public class PersonaDAO extends BaseDAO<Persona> {
+
+	public Persona findByUsuario(Integer idUsuario) {
+		Search s = new Search();
+
+		s.addFilterEqual("fkUsuario.idUsuario", idUsuario);
+
+		return searchUnique(s);
+	}
 
 }
