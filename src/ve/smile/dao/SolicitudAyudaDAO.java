fa8f61ece9bcb41be0a17afbc770759352e2ce31
RosireMaiz <rosire08@gmail.com>
2016-10-23 16:05:43 -0400

    Modificaciones para servicio de reporte de solicitudes de ayuda

--- src/ve/smile/dao/SolicitudAyudaDAO.java
+++ src/ve/smile/dao/SolicitudAyudaDAO.java
@@ -1,8 +1,18 @@
 package ve.smile.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import lights.core.googlecode.genericdao.dao.jpa.BaseDAO;
 import ve.smile.dto.SolicitudAyuda;
 
 public class SolicitudAyudaDAO extends BaseDAO<SolicitudAyuda> {
+
+	@SuppressWarnings("unchecked")
+	public List<SolicitudAyuda> consultaSolicitudesParametrizado(String sql) {
+		Query query = createQuery(sql);
+		return query.getResultList();
+	}
 
 }
