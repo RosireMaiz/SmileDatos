64d856f5c3d0beaaefde5c964a9632ca63d421bb
jorgeavenda <jorgeavenda.11@gmail.com>
2016-10-24 23:58:30 -0400

    para el envio de correo

--- src/ve/smile/enums/PropiedadEnum.java
+++ src/ve/smile/enums/PropiedadEnum.java
@@ -3,7 +3,9 @@ package ve.smile.enums;
 import java.util.ResourceBundle;
 
 public enum PropiedadEnum {
-	ORGANIZACION, DONATIVO, INFO_MOVIL, APK;
+	ORGANIZACION, DONATIVO, INFO_MOVIL, APK, EMAIL_COMENTARIO_ALBUM,
+	EMAIL_CONTACTANOS, EMAIL_FAQ, EMAIL_BUZON, EMAIL_POSTULACION_VOLUNTARIO,
+	EMAIL_POSTULACION_PADRINO;
 
 	@Override
 	public String toString() {
